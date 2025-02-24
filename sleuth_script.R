4c16ff98a6db24e0f5764a44b028ece565c77f0d
Tyler Jensen <tjensen1115@gmail.com>
2025-02-23 20:22:38 -0600

    Added comments

--- sleuth_script.R
+++ sleuth_script.R
@@ -1,16 +1,20 @@
-library('sleuth')
-stab = read.table("samples.txt",header=TRUE)
-so = sleuth_prep(stab)
+library('sleuth') # load sleuth library
+stab = read.table("samples.txt",header=TRUE) # read table
+so = sleuth_prep(stab) # prepare sleuth object using table
 
-so = sleuth_fit(so, ~condition, 'full')
-so = sleuth_fit(so, ~1, 'reduced')
-so = sleuth_lrt(so, 'reduced', 'full')
+so = sleuth_fit(so, ~condition, 'full') # fit full model with condition as explanatory variable
+so = sleuth_fit(so, ~1, 'reduced') # fit a reduced model
+so = sleuth_lrt(so, 'reduced', 'full') # likelihood ratio test to compare reduced and full model
 
-library(dplyr)
+library(dplyr) # load dplyr library
 
-sleuth_table = sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
-sleuth_significant = dplyr::filter(sleuth_table, qval <= 0.05) |> dplyr::arrange(pval)
-head(sleuth_significant, n = 10)
+sleuth_table = sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE) # get the results from test above
 
+# filter significant results with an adjusted p-value (q-value) threshold of 0.05
+# sort the results in ascending order of raw p-values
+sleuth_significant = dplyr::filter(sleuth_table, qval <= 0.05) |> dplyr::arrange(pval) 
+head(sleuth_significant, n = 10) # view only first ten
+
+# write the table to a text file with only target_id, pval, and qval and top 10 results
 write.table(sleuth_significant, file="sleuth_results.txt",quote = FALSE,row.names = FALSE)
 head(dplyr::select(sleuth_significant, target_id, pval, qval), n=10)
